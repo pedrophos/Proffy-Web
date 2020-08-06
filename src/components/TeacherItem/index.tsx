b76d0021d5277d13961c8a2253f9f14c23bd7a19
unknown <pedrohenriquedarck@hotmnail.com>
2020-08-06 11:51:53 -0300

    Finalização da listagem de teachers.

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,35 +3,55 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string;
+    subject: string;
+    whatsapp: string;  
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
   return(
     <article className="teacher-item">
     <header>
       <img 
-        src="https://avatars1.githubusercontent.com/u/25045150?s=460&u=3339f7342fc7487e816a7307bbfd7e5ef9fc8a88&v=4" 
-        alt="Pedro Henrique" />
+        src={teacher.avatar}
+        alt={teacher.name} />
         <div>
-          <strong>Pedro Henrique</strong>
-          <span>Física</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
     </header>
 
-    <p>
-      Descrição da materia informada pelo professor
-      <br /><br />
-      continuação da descrição da materia
-    </p>
+    <p>{teacher.bio} </p>
 
     <footer>
       <p>
         Preço por hora/aula
-        <strong>R$ 50,00</strong>
+        <strong>R$ {teacher.cost}</strong>
       </p>
-      <button type="button">
+      <a
+      // eslint-disable-next-line
+        target="_blank"
+        onClick={createNewConnection} 
+        href={`https://wa.me/${teacher.whatsapp}`}>
         <img src={whatsappIcon} alt="Whatsapp" />
         Entrar em contato
-      </button>
+      </a>
     </footer>
   </article>
   );
